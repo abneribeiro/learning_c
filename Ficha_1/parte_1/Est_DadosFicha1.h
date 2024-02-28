32314d0a67181a10c0f4a19c4b2b2546ef060764
seu nome <eliasabner38@gmail.com>
2024-02-28 12:30:39 +0000

    feat: Refactor function prototypes and fix typos

--- Ficha_1/parte_1/Est_DadosFicha1.h
+++ Ficha_1/parte_1/Est_DadosFicha1.h
@@ -51,27 +51,27 @@ typedef struct
 
 
 // ---------------------------------------------------------------------
-//protótipos das funcoes implementadas
+//protï¿½tipos das funcoes implementadas
 
-void GerarFicheiro_So_Para_Testes(char *nficheiro, int N); // gerar aleatoriamente N registos de USERS e gravar no ficheiro “Utilizadores.dat”
+void GerarFicheiro_So_Para_Testes(char *nficheiro, int N); // gerar aleatoriamente N registos de USERS e gravar no ficheiro ï¿½Utilizadores.datï¿½
 /* **********************************************************
 // ALUNOS: implementar!!
-// alterar ff para devolver int com indicação de sucesso/insucesso ao ler os dados
+// alterar ff para devolver int com indicaï¿½ï¿½o de sucesso/insucesso ao ler os dados
 // int LerFicheiro(EDADOS *ed, char *nficheiro);
  // **********************************************************
 */
-void LerFicheiro(EDADOS *ed, char *nficheiro); // lê os dados do ficheiro
+void LerFicheiro(EDADOS *ed, char *nficheiro); // lï¿½ os dados do ficheiro
 EDADOS * DuplicarDados(EDADOS *ed); // duplica os dados
-void OrdenarDados(EDADOS *ed); // Deve implementar um método de ordenação
+void OrdenarDados(EDADOS *ed); // Deve implementar um mï¿½todo de ordenaï¿½ï¿½o
 void ListarUtilizadores(EDADOS *ed); // Deve Listar todos os utilizadores
-int ContarPessoasAcessosAno(EDADOS *ed, int ano); // Contar o número de pessoas que fizeram o último acesso ao site num dado ano
+void ContarPessoasAcessosAno(EDADOS *ed, int ano); // Contar o nï¿½mero de pessoas que fizeram o ï¿½ltimo acesso ao site num dado ano
 void PessoaMaisAcessos(EDADOS *ed); // Determinar a pessoa que fez mais acessos
-float TotalJoias(EDADOS *ed); // Determinar a soma de todas as “joias”
-int MesMaisRegistos(EDADOS *ed); // Determinar qual o mês em que houve mais registos
-void PesquisarCod(EDADOS *ed, char *UserName); // Pesquisar pelo código
+float TotalJoias(EDADOS *ed); // Determinar a soma de todas as ï¿½joiasï¿½
+int MesMaisRegistos(EDADOS *ed); // Determinar qual o mï¿½s em que houve mais registos
+void PesquisarCod(EDADOS *ed, char *UserName); // Pesquisar pelo cï¿½digo
 void PesquisarNome(EDADOS *ed, char *Name); // Pesquisar pelo nome
 
-void LibertarMemoria(EDADOS *ed); // Deve libertar toda a memória alocada
+void LibertarMemoria(EDADOS *ed); // Deve libertar toda a memï¿½ria alocada
 
 
 
